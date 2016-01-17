cefb761d981b5c2be0f134c77760b494e345c267
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2016-01-17 17:24:47 +0100

    app: API addition
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- src/mongoql.erl
+++ src/mongoql.erl
@@ -1,12 +1,34 @@
 -module('mongoql').
 
 %% API exports
--export([]).
+-export([parse/1]).
+
+-export_type([query/0]).
+
+%%%_ * Types -----------------------------------------------------------
+
+-type query() :: list() | binary().
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
+% init() ->
+%   % compile lexer
+%   leex:file(lexer),
+%   c(lexer),
+%   % compile parser
+%   yecc:file(parser),
+%   c(parser).
+
+-spec parse(query()) -> {ok, list()} | {error, unknow_input}.
+parse(Query) when is_binary(Query) ->
+  parse(binary_to_list(Query));
+parse(Query) when is_list(Query) ->
+  {ok, Tokens, _} = lexer:string(Query),
+  parser:parse(Tokens);
+parse(_Query) ->
+  {error, unknow_input}.
 
 %%====================================================================
 %% Internal functions
